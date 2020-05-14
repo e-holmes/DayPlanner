b20e4cb36083598c42a28ea897c0db2415d1374f
e-holmes <bethholmes768@gmail.com>
2020-05-13 19:56:49 -0700

    Getting basic's set up for user log in and out

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-import Navbar from "../src/components/Navbar";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import User from "./pages/User";
 
